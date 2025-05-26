47d5941c38f27d9e7a24034552a9d887cee53d46
Jonathan-creator111 <hapsjune@gmail.com>
2025-05-26 15:31:00 +0300

    mydstask

--- mydstask.py
+++ mydstask.py
@@ -1,24 +1,23 @@
 #Create a file called mydstask.py and attempt the below questions:
 # my_ds = [23, “Jane”, (560), [“Lesson”, “Maths”, {“currency” : “KES”}], 987, (76,”John”)]
-my_ds = "[23, “Jane”, (560), [“Lesson”, “Maths”, {“currency” : “KES”}], 987, (76,”John”)]"
+my_ds = [23, "Jane", (560), ["Lesson", "Maths", {"currency" : "KES"}], 987, (76,"John")]
 #1. Print KES
-print(type(my_ds))
-a=my_ds.find("KES")
-print(a)
-print(my_ds[55:57+1])
+print(my_ds[3][2]["currency"])
 #2. Print 560
-print(type(my_ds))
-b=my_ds.find("560")
-print(b)
-print(my_ds[14:16+1])
+print(my_ds[2])
 #3. Print Maths
-print(type(my_ds))
-c=my_ds.find("Maths")
-print(c)
-print(my_ds[32:36+1])
+print(my_ds[3][1])
 #4. In the dictionary with the key currency, add another key “amount” with value 90
+my_ds[3][2]["amount"]=90
+print(my_ds)
 #5. Reverse 987 to 789 without using an inbuilt -method or Assigning 789 manually.
     #Hint: Strings can be reversed using [::]
+my_ds[4]=str(my_ds[4])
+my_ds[4]=my_ds[4][::-1]
+my_ds[4]=int(my_ds[4])
+print(my_ds)
 #6. Change the name “John” to “Jane” .
-print(type(my_ds))
-print(my_ds.replace("John","Jane"))
+my_ds[5]=list(my_ds[5])
+my_ds[5][1]="Jane"
+my_ds[5]=tuple(my_ds[5])
+print(my_ds)
